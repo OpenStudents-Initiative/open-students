bcbdccac8bec66f7c7cdd802ba67da14a29f0c3b
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2024-02-12 12:37:15 -0500

    Created folder structure

--- open-students-back/supabase/migrations/20231224032838_remote_schema.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."university" add column "nickname" character varying;
-
-alter table "public"."university" alter column "created_at" drop default;
-
-alter table "public"."university" alter column "created_at" drop not null;
-
-
